d21a64958734d91cc69996764b4f3c6dc3e53b2f
uspehovmax <uspehovmax@mail.ru>
2022-05-28 16:42:40 +0300

    Add new files, change text

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,4 +3,14 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.post_list_item)
+
+
+/**
+ * @tools:sample/lorem/random[0]
+ *
+ * app:srcCompat="@tools:sample/avatars"
+tools:src="@tools:sample/avatars"
+tools:srcCompat="@drawable/ic_launcher_foreground_36" />
+
+ */
