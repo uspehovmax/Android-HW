699c96cccea02b6060c7a1e4f8715ae27fca358d
uspehovmax <uspehovmax@mail.ru>
2022-07-02 13:13:10 +0300

    add files, change code - crud task

---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostInteractionListener.kt
@@ -0,0 +1,11 @@
+package ru.netology.nmedia.adapter
+
+import ru.netology.nmedia.dto.Post
+
+interface PostInteractionListener {
+    fun onLikeClicked(post: Post)
+    fun onRemoveClicked(post: Post)
+    fun onShareClicked(post: Post)
+    fun onEditClicked(post: Post)
+
+}
